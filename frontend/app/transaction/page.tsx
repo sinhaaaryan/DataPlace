6352cea9a77e68a568c14ff7932e72b20b561cb9
aaryansinha <sinha.aar@northeastern.edu>
2023-10-22 01:51:03 -0400

    buyer form done + transaction page empty

---
+++ frontend/app/transaction/page.tsx
@@ -0,0 +1,185 @@
+"use client"
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import {useEffect, useState} from 'react';
+import { useRouter } from 'next/navigation';
+import {Dialog, DialogTrigger} from "@radix-ui/react-dialog";
+import {DialogContent, DialogDescription, DialogHeader, DialogTitle} from "@/components/ui/dialog";
+
+
+
+
+
+export default ({action = '/search'}) => {
+
+
+
+    return (
+
+
+        <>
+            <div className="z-10 w-full max-w-xl px-5 xl:px-0">
+
+
+            </div>
+
+        </>
+    );
+}
+
+// const [message, setMessage] = useState<boolean>(false);
+//
+//
+//
+// const onButtonClickHandler = () => {
+//     console.log("before",message)
+//     setMessage(true);
+//
+//     console.log("after",message)
+// };
+
+
+{/*<Label htmlFor="cName">Company Name</Label>*/}
+{/*<Input type="text" id="cName" placeholder="" />*/}
+{/*<Label htmlFor="dType">Data Type</Label>*/}
+{/*<Input type="text" id="dType" placeholder="Activity Data, Athlete Data" />*/}
+{/*<Label htmlFor="price">Price</Label>*/}
+{/*<Input type="number" id="price" placeholder="0.00" />*/}
+{/*    <Label htmlFor="pKey">Public Key</Label>*/}
+{/*    <Input type="number" id="pKey" placeholder="" />*/}
+{/*    <Label htmlFor="nB">buyN</Label>*/}
+{/*    <Input type="number" id="nB" placeholder="" />*/}
+
+
+
+{/*    <Link href="../transaction">*/}
+
+{/*      <button className="bg-blue-500 text-white px-4 py-2 mt-2 rounded-md hover:bg-blue-600 cursor-pointer">*/}
+{/*        Submit*/}
+{/*      </button>*/}
+
+{/*    </Link>*/}
+
+
+{/*<form onSubmit={handleFormSubmit}>*/}
+{/*  <label>*/}
+{/*    Username:*/}
+{/*    <input*/}
+{/*        type="text"*/}
+{/*        name="username"*/}
+{/*        value={formData.username}*/}
+{/*        onChange={handleInputChange}*/}
+{/*    />*/}
+{/*  </label>*/}
+{/*  /!* other form fields *!/*/}
+{/*  <button type="submit">Submit</button>*/}
+{/*</form>*/}
+
+
+
+
+// "use client"
+// import { Label } from "@/components/ui/label";
+// import { Input } from "@/components/ui/input";
+// import Link from "next/link";
+// import { useState } from 'react';
+// import { useRouter } from 'next/navigation';
+//
+//
+// // eslint-disable-next-line react/display-name
+// export default () => {
+//
+//
+//
+//
+//     return (
+//
+//         <div>
+//                 {/*<h1*/}
+//                 {/*    className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"*/}
+//                 {/*    style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}*/}
+//                 {/*>*/}
+//                 {/*    Thank you for your transaction!*/}
+//
+//                 {/*    <br/>*/}
+//
+//                 {/*    Your previous orders:*/}
+//                 {/*</h1>*/}
+//                 {/*<br/>*/}
+//
+//             <p>
+//
+//                 Hello test
+//             </p>
+//
+//
+//             {/*<tbody>*/}
+//             {/*<tr>ciao</tr>*/}
+//             {/*<td>ciao111</td>*/}
+//             {/*<tr><tr></tr></tr>*/}
+//             {/*</tbody>*/}
+//
+//             {/*<p>Hello Test ewyiagruiwabuirbawiurbawiurbwaiubr</p>*/}
+//
+//             <div>
+//                 {/*<table>*/}
+//                 {/*    <thead>fds*/}
+//                 {/*    </thead>*/}
+//                 {/*    <caption>Alien football stars</caption>*/}
+//
+//                 {/*    <tr>*/}
+//                 {/*        <th scope="row">Mia Oolong</th>*/}
+//                 {/*        <td>9</td>*/}
+//                 {/*        <td>6,219</td>*/}
+//                 {/*    </tr>*/}
+//
+//                 {/*</table>*/}
+//
+//             </div>
+//
+//         </div>
+//
+//
+//
+//     );
+// }
+//
+//
+//
+//
+// {/*<Label htmlFor="cName">Company Name</Label>*/}
+// {/*<Input type="text" id="cName" placeholder="" />*/}
+// {/*<Label htmlFor="dType">Data Type</Label>*/}
+// {/*<Input type="text" id="dType" placeholder="Activity Data, Athlete Data" />*/}
+// {/*<Label htmlFor="price">Price</Label>*/}
+// {/*<Input type="number" id="price" placeholder="0.00" />*/}
+// {/*    <Label htmlFor="pKey">Public Key</Label>*/}
+// {/*    <Input type="number" id="pKey" placeholder="" />*/}
+// {/*    <Label htmlFor="nB">buyN</Label>*/}
+// {/*    <Input type="number" id="nB" placeholder="" />*/}
+//
+//
+//
+// {/*    <Link href="../transaction">*/}
+//
+// {/*      <button className="bg-blue-500 text-white px-4 py-2 mt-2 rounded-md hover:bg-blue-600 cursor-pointer">*/}
+// {/*        Submit*/}
+// {/*      </button>*/}
+//
+// {/*    </Link>*/}
+//
+//
+// {/*<form onSubmit={handleFormSubmit}>*/}
+// {/*  <label>*/}
+// {/*    Username:*/}
+// {/*    <input*/}
+// {/*        type="text"*/}
+// {/*        name="username"*/}
+// {/*        value={formData.username}*/}
+// {/*        onChange={handleInputChange}*/}
+// {/*    />*/}
+// {/*  </label>*/}
+// {/*  /!* other form fields *!/*/}
+// {/*  <button type="submit">Submit</button>*/}
+// {/*</form>*/}
